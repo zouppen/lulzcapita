906e86eeba16b678e4a879329c802919f3a33acc
Joel Lehtonen <joel.lehtonen@iki.fi>
2012-01-02 03:08:40 +0200

    Fixed User.hs for new database schema #1. Improved logging #5.

--- User.hs
+++ User.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE RecordWildCards #-}
+
+-- |This dumps user info for Greasemonkey script. This provides very
+-- much "Nordnet only" data at the moment.
 module User where
 
 import Data.ConfigFile (ConfigParser)
@@ -11,7 +14,7 @@ import DatabaseTools
 userInfo :: ConfigParser -> CGI CGIResult
 userInfo conf = do
   info <- getPortfolioHeaders conf 
-  json <- liftIO $ runCouchDBURI (peek conf "secret.db") $ readDB info
+  json <- lulzCouch conf $ readDB info
   output $ encode json
 
 -- |Extracts the information from databases. The portfolio parameter
@@ -20,27 +23,29 @@ readDB :: PortfolioInfo -> CouchMonad (JSObject JSValue)
 readDB PortfolioInfo{..} = do
   -- Should return only one row.
   timeRaw <- queryView (peek conf "location.db")
-             (doc "couchapp") (doc "lastsync")
+             (doc "couchapp") (doc "last_sync")
              [("key",jsonHash)
              ,("group",showJSON True)
              ]
 
   -- If there is some data, return it, otherwise the default
-  let timestamp = case timeRaw of
-        [(_,x)] -> x
-        []      -> showJSON "2000-01-01"  -- Start of the time
+  let (isFirst,timestamp) = case timeRaw of
+        [(_,x)] -> (False,x)
+        []      -> (True,showJSON "2000-01-01")  -- Start of the time.
 
   -- Get nick of that user.
-  userRaw <- queryView (peek conf "database_names.user")
-             (doc "couchapp") (doc "portfolio")
+  userRaw <- queryView (peek conf "location.db")
+             (doc "couchapp") (doc "portfolio_user")
              [("key",jsonHash)]
-
-  logCGI $ show jsonHash ++ " " ++ show userRaw -- Miksei löydy....
 
   -- If there is some data, return it, otherwise the default.
   userDoc <- case userRaw :: [(Doc,JSValue)] of
-    [(doc,_)] -> getDoc (peek conf "database_names.user") doc
-    []        -> return Nothing
+    [(doc,_)] -> do
+      logCGI $ "User " ++ show doc ++ " requests info"
+      getDoc (peek conf "location.db") doc
+    [] -> do
+      logCGI $ "Unknown user looking for portfolio " ++ hash []
+      return Nothing
     
   -- Now get the doc name, putting it to returned value.
   let userjson = case userDoc of
@@ -48,6 +53,7 @@ readDB PortfolioInfo{..} = do
         Nothing      -> JSNull
 
   return $ toJSObject [("last",timestamp)
+                      ,("is_first",showJSON isFirst)
                       ,("user",userjson)
                       ]
 
